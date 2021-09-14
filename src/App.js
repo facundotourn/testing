d927176eb27a2c421f7117065504c70c4276c43a
maxi <maxy_caseres4@hotmail.com>
2021-09-13 21:09:40 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [cantLineas, setcantLineas] = useState(null)
+
+const handleSubmit = (e) =>{
+  e.preventDefault()
+  const lineas = document.getElementById("code").value.split("\n").length
+  //console.log(document.getElementById("code").value.split("\n").lenght)
+  setcantLineas(lineas)
+  console.log(lineas)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">    <img src="https://miel.unlam.edu.ar/vista/imagenes/logo_unlam_34.png" className="unlam-logo" alt="logo unlam"/> </header>
+
+
+
+      <div className="contenedor">
+    <div>
+      <div className="codigo-container">
+        GRUPO 3
+        <textarea id="code" placeholder="codigo a evaluar" className="codigo" />
+
+      </div>
+
+  
+        <div className="resultado">
+
+
+
+        <p className="label"> resultado 1</p>
+          <input value={cantLineas} readOnly={true}/>
+
+
+
+        </div>
+        <div className="resultado">
+
+
+
+        <p className="label"> resultado 2</p>
+        <input readOnly={true}/>
+
+
+
+        </div>
+        <div className="resultado">
+
+
+
+        <p className="label"> resultado 3</p>
+        <input readOnly={true}/>
+
+
+
+        </div>
+        <div className="resultado">
+
+
+
+        <p className="label"> resultado 4</p>
+        <input readOnly={true}/>
+
+
+
+        </div>
+
+
+        <button onClick={handleSubmit} class="button">Calcular</button>
+      </div>
+      </div>
     </div>
   );
 }
